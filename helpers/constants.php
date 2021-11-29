6ebc8777856c634920c36b4ac3236dfec950549a
nevadskiy <nevadskiy@gmail.com>
2021-11-29 12:49:11 +0200

    Rename const

--- helpers/constants.php
+++ helpers/constants.php
@@ -1,5 +1,5 @@
 <?php
 
-if (! defined('ROUTE_UUID_PATTERN')) {
-    define('ROUTE_UUID_PATTERN', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
+if (! defined('UUID_ROUTE_PATTERN')) {
+    define('UUID_ROUTE_PATTERN', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
 }
